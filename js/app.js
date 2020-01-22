ba82d84550c7f893ec9afa8b883630aab7b9209f
Salma Samir <salmashoair@gmail.com>
2020-01-22 20:54:34 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-//js
+
+$('.scene')
+.on('click', '#nextBtn', nextPage)
+.on('click', '#prevBtn', prevPage);
+
+// $('.book').hammer().on("swipeleft", nextPage);
+// $('.book').hammer().on("swiperight", prevPage);
+
+function prevPage() {
+  $('.flipped')
+    .last()
+    .removeClass('flipped')
+    .addClass('active')
+    .siblings('.page')
+    .removeClass('active');
+}
+function nextPage() {
+  $('.active')
+    .removeClass('active')
+    .addClass('flipped')
+    .next('.page')
+    .addClass('active')
+    .siblings();
+}
+
+document.addEventListener("keydown" , function(e){
+
+    if(e.keyCode == 39)
+    {
+        nextPage();
+    }
+    else if(e.keyCode == 37)
+    {
+        prevPage();
+    }
+})
